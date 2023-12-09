9542dacb00ce5f9d54ff42b27d1b15af5dfe2fa3
Razalp <razalp0012300@gmail.com>
2023-12-09 10:24:05 +0530

    set up react and node js and express

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,22 @@
 const express=require('express')
-
-
+const {chats} =require('./data')
+const dotenv =require('dotenv')
+dotenv.config();
+const PORT =process.env.PORT || 5000
 const app=express()
 
 
-app.listen(5000,console.log('server run on port 5000'))
+app.get('/',(req,res)=>{
+    res.send('api is runing')
+})
+app.get('/chat',(req,res)=>{
+    res.send(chats)
+})
+
+app.get('/chat/:id',(req,res)=>{
+    const singleChat =chats.find((c)=>c._id===req.params.id)
+    res.send(singleChat)
+})
+
+
+app.listen(PORT,console.log('server run on port 5000'))
