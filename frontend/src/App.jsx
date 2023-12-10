cc9b634d277778f8cb1c206cd3cee5c571195f21
Razalp <razalp0012300@gmail.com>
2023-12-10 16:10:04 +0530

    login page setting up

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 
-import './App.css';
+import './App.css'; 
 import Home from './componets/Home';
 import Chat from './componets/Chat';
 
@@ -11,10 +11,12 @@ const App = () => {
   return (
   
       // <h1>hwello</h1>
+      <div className='App'>
      <Routes>
         <Route path='/' element={<Home/>} /> 
         <Route path='/chat' element={<Chat/>}/>
         </Routes>
+        </div>
   
   );
 }
--- frontend/src/componets/Home.jsx
+++ frontend/src/componets/Home.jsx
@@ -1,10 +1,40 @@
+import { Box, Container,Text , Tabs, TabList, TabPanels, Tab, TabPanel } from '@chakra-ui/react';
 import React from 'react';
 
 const Home = () => {
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <Container maxW='xl' centerContent>
+      
+      <Box
+      className='chatbg'
+      d='flex'
+      justifyContent='center'
+      p={3}
+      w='100%'
+      m='40px 0 15px 0'
+      borderRadius='lg'
+      borderWidth='1px'
+      >
+        <Text fontSize='4xl' fontFamily='work sans' color='black' textAlign='center'><b>Chat-Make-Friends</b></Text>
+        </Box>
+        <Box bg='white' w='100%' p={4} borderRadius='lg' borderWidth='1px' color='black' >
+      <Tabs variant='enclosed'>
+  <TabList>
+    <Tab>One</Tab>
+    <Tab>Two</Tab>
+  </TabList>
+  <TabPanels>
+    <TabPanel>
+      <p>one!</p>
+    </TabPanel>
+    <TabPanel>
+      <p>two!</p>
+    </TabPanel>
+  </TabPanels>
+</Tabs>
+        </Box>
+        
+    </Container>
   );
 }
 
