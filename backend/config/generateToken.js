053d70c913521ab018f7d5ef05b405ac67dd6147
Razalp <razalp0012300@gmail.com>
2024-01-02 11:54:13 +0530

    changes

--- backend/config/generateToken.js
+++ backend/config/generateToken.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken');
 
 const generateToken = (userId) => {
-    // Assuming userId is the information you want to encode in the token
+
     return jwt.sign({ id: userId }, process.env.JWT_SECRET, {
         expiresIn: '15d'
     });
